8461ced98abb527b20e7a9a0dd28e77ea57214f7
Saad Srabon <saadsrabon2@gmail.com>
2025-06-02 13:41:37 +0600

    button added

--- src/components/iconButton.jsx
+++ src/components/iconButton.jsx
@@ -3,12 +3,31 @@
 // hover property hbe
 // difrent color hbe
 
+import { cva } from "class-variance-authority"
+import clsx from "clsx"
 import { Component } from "react"
 
+const buttonStyles =cva('',{
+    variants: {
+        size: {
+            sm: 'text-sm',
+            md: 'text-md',
+            lg: 'text-lg',
+        },
+        color: {
+            primary: 'bg-primary text-white',
+            secondary: 'bg-secondary text-white',
+        },
+    },
+    defaultVariants: {
+        size: 'md',
+        color: 'primary',
+    },
+})
 
-const IconButton = ({children}) => {
+const IconButton = ({children,size,color,className}) => {
     return (
-        <Component>
+        <Component className={clsx(buttonStyles({size, color, className}))}>
             {children}
         </Component>
     )
