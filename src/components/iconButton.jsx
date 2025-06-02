afd8d70a2070aff5dd78cded1503a71e729598cd
Saad Srabon <saadsrabon2@gmail.com>
2025-06-02 12:35:14 +0600

    Base Icon Button added

---
+++ src/components/iconButton.jsx
@@ -0,0 +1,18 @@
+//  Button banabo.
+// Jeitar diffrent size hbe
+// hover property hbe
+// difrent color hbe
+
+import { Component } from "react"
+
+
+const IconButton = ({children}) => {
+    return (
+        <Component>
+            {children}
+        </Component>
+    )
+}
+
+
+export default IconButton
