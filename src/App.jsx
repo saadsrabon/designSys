e27fd02e531f8dab1578cf71bd4543c6f0d2c5fa
Saad Srabon <saadsrabon2@gmail.com>
2025-06-02 13:48:05 +0600

    button cursor added

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import Card from './components/card'
+import IconButton from './components/iconButton'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -14,6 +15,7 @@ function App() {
       Array(10).fill(0).map((_, index) => (
         <Card key={index} variant="bordered" hoverable={false}  >
           Card {index + 1}
+          <IconButton size={"sm"} color={"primary"} > Hello </IconButton>
         </Card>
       ))
      }
--- src/components/iconButton.jsx
+++ src/components/iconButton.jsx
@@ -7,7 +7,7 @@ import { cva } from "class-variance-authority"
 import clsx from "clsx"
 import { Component } from "react"
 
-const buttonStyles =cva('',{
+const buttonStyles =cva(' max-w-[100px] flex items-center justify-center gap-2 px-4 py-2 rounded-full cursor-pointer',{
     variants: {
         size: {
             sm: 'text-sm',
@@ -15,7 +15,7 @@ const buttonStyles =cva('',{
             lg: 'text-lg',
         },
         color: {
-            primary: 'bg-primary text-white',
+            primary: 'bg-red-400 text-white',
             secondary: 'bg-secondary text-white',
         },
     },
@@ -27,9 +27,9 @@ const buttonStyles =cva('',{
 
 const IconButton = ({children,size,color,className}) => {
     return (
-        <Component className={clsx(buttonStyles({size, color, className}))}>
+        <div as='button' className={clsx(buttonStyles({size, color, className}))}>
             {children}
-        </Component>
+        </div>
     )
 }
 
